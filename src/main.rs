6e91a7bed054b094bea394bf8b95b62955435fb5
Christian Todd <christian.todd7@gmail.com>
2021-04-10 21:00:59 -0400

    prefer 1024 kb size & embrace more pattern matching enums.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::path::PathBuf;
 use structopt::clap::arg_enum;
 use structopt::StructOpt;
 
-mod byte_formatter {
+mod byte_fmt {
     use std::cmp;
 
+    /// Forked from https://github.com/banyan/rust-pretty-bytes/blob/b164c61b87b633cc175e02b6fddc7d28fcf6b9c7/src/converter.rs
     pub fn pretty(num: f64) -> String {
         let negative = if num.is_sign_positive() { "" } else { "-" };
         let num = num.abs();
@@ -15,7 +16,7 @@ mod byte_formatter {
         if num < 1_f64 {
             return format!("{}{} {}", negative, num, "B");
         }
-        let delimiter = 1000_f64;
+        let delimiter = 1024_f64;
         let exponent = cmp::min(
             (num.ln() / delimiter.ln()).floor() as i32,
             (units.len() - 1) as i32,
@@ -35,12 +36,12 @@ mod compress {
 
     use std::io;
     use std::io::prelude::*;
-    pub fn br_compress(input: &[u8]) -> Vec<u8> {
+    pub fn brotli(input: &[u8]) -> Vec<u8> {
         let mut writer = brotli::CompressorWriter::new(Vec::new(), 4096, 11, 22);
         writer.write_all(input).unwrap();
         writer.into_inner()
     }
-    pub fn gz_compress(input: &[u8]) -> io::Result<Vec<u8>> {
+    pub fn gzip(input: &[u8]) -> io::Result<Vec<u8>> {
         let mut gz_enc = ZlibEncoder::new(Vec::new(), Compression::best());
         gz_enc.write_all(input)?;
         gz_enc.finish()
@@ -48,10 +49,11 @@ mod compress {
 }
 
 arg_enum! {
-    #[derive(Debug)]
+    #[derive(Copy,Clone,Debug)]
     enum CompressionKind {
         Gz,
         Br,
+        Raw,
         All
     }
 }
@@ -67,32 +69,60 @@ struct Cli {
 }
 
 fn main() -> io::Result<()> {
-    let opt = Cli::from_args();
+    let args = Cli::from_args();
 
-    for file in opt.files {
+    for file in args.files {
         let contents = read_input_mod(&file)?;
-        let sizes = pretty_sizes(get_length(contents)?);
-
-        println!("sizes: {:?}", sizes);
+        match get_sizes(contents, args.kind)? {
+            CompressionResult::All { raw, br, gz } => {
+                println!("raw: {}", byte_fmt::pretty(raw as f64));
+                println!("br: {}", byte_fmt::pretty(br as f64));
+                println!("gz: {}", byte_fmt::pretty(gz as f64));
+            }
+            CompressionResult::Br(b) | CompressionResult::Gz(b) | CompressionResult::Raw(b) => {
+                println!("{:?}: {}", args.kind, byte_fmt::pretty(b as f64));
+            }
+        };
     }
 
     Ok(())
 }
 
-fn pretty_sizes(x: (usize, usize, usize)) -> (String, String, String) {
-    (
-        byte_formatter::pretty(x.0 as f64),
-        byte_formatter::pretty(x.1 as f64),
-        byte_formatter::pretty(x.2 as f64),
-    )
+enum CompressionResult {
+    Gz(usize),
+    Br(usize),
+    Raw(usize),
+    All { gz: usize, br: usize, raw: usize },
 }
 
-fn get_length(x: Vec<u8>) -> io::Result<(usize, usize, usize)> {
-    let raw_size = x.len();
-    let br_comp_res = compress::br_compress(&x);
-    let gz_comp_res = compress::gz_compress(&x.clone())?;
+fn get_sizes(x: Vec<u8>, k: CompressionKind) -> io::Result<CompressionResult> {
+    match k {
+        CompressionKind::Gz => {
+            let gz_b = compress::gzip(&x.clone())?;
 
-    Ok((raw_size, gz_comp_res.len(), br_comp_res.len()))
+            Ok(CompressionResult::Gz(gz_b.len()))
+        }
+        CompressionKind::Br => {
+            let br_b = compress::brotli(&x);
+
+            Ok(CompressionResult::Br(br_b.len()))
+        }
+        CompressionKind::Raw => {
+            let raw_b = x.len();
+
+            Ok(CompressionResult::Raw(raw_b))
+        }
+        CompressionKind::All => {
+            let br = compress::brotli(&x).len();
+            let gz = compress::gzip(&x.clone())?.len();
+            let raw = x.len();
+            Ok(CompressionResult::All {
+                br: br,
+                gz: gz,
+                raw: raw,
+            })
+        }
+    }
 }
 
 fn read_input_mod(p: &PathBuf) -> io::Result<Vec<u8>> {
